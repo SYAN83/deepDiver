7fb3e6b156e70da87c122a3fb256f8c214a50723
Shu Yan <yanshu.usc@gmail.com>
2017-02-04 19:47:05 -0500

    log to file

--- deepDiver/spiders/deep_diver.py
+++ deepDiver/spiders/deep_diver.py
@@ -1,29 +1,36 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.exceptions import CloseSpider
+import logging
+from scrapy.utils.log import configure_logging
 
 
 class DeepSpider(CrawlSpider):
 
-    name = 'physics'
-    allowed_domains = ['physics.sc.edu']
-    start_urls = ['http://www.physics.sc.edu']
+    name = 'get_email'
+    allowed_domains = ['harvard.edu']
+    start_urls = ['http://www.harvard.edu']
     count = 10
     rules = (
-        Rule(LinkExtractor(allow=(), deny=('robots\.txt',)), callback="parse_items", follow=True),
+        Rule(LinkExtractor(allow=(), deny=('robots.txt',)), callback='parse_items', follow=True),
     )
 
-    # def __init__(self, start_urls, n=-1):
-    #
-    # def start_requests(self):
-    #     for url in self.start
+    def __init__(self, *args, **kwargs):
+        log_setting = {
+            'LOG_FILE': 'crawl.log',
+            'LOG_LEVEL': logging.DEBUG
+        }
+        configure_logging(settings=log_setting, )
+        super(DeepSpider, self).__init__(*args, **kwargs)
 
     def parse_items(self, response):
         if response.status / 10 == 20:
-            self.logger.info(msg='\n\tON SUCCESS: {0}'.format(response.url,))
+            self.logger.info(msg=u'ON SUCCESS: {0}'.format(response.url,))
         if response.status / 10 == 40:
-            self.logger.error(msg='\n\tCLIENT ERROR: {0}'.format(response.url, ))
+            pass
+            self.logger.error(msg=u'CLIENT ERROR: {0}'.format(response.url, ))
         # print response.url
         self.count -= 1
         if self.count == 0:
+            # self.logfile.close()
             raise CloseSpider('bandwidth_exceeded')
