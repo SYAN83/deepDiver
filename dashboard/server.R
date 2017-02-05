e1e9cb6107438dbd4811b181018eff01c9d9af0a
Shu Yan <yanshu.usc@gmail.com>
2017-02-04 19:45:46 -0500

    test system2

--- dashboard/server.R
+++ dashboard/server.R
@@ -1,5 +1,11 @@
 library(shiny)
 
+CMD <- "/home/shu/anaconda2/bin/scrapy"
+CRAWL <- "crawl physics"
+
+
 function(input, output, session) {
-  
+  observeEvent(input$crawl, {
+    system2(command = CMD, args = CRAWL, stdout = TRUE)
+  })
 }
